500bb7058236ed2f8b794cbc0c7dc77a39c059fe
Rafael <rafaelealdias@gmail.com>
2016-03-28 21:59:58 -0300

    to mUA/year

--- Main.m
+++ Main.m
@@ -1,14 +1,24 @@
+clc;
 t0 = 0;
-tf = 1000;
+tf = 70;
 n = 100;
 dt = (tf - t0)/n;
 G = 6.674287*10.^(-11);
 M = 1.9891*10.^30;
 Y = zeros(4, n);
+GM = G*M ;%[m^3/(kg*s^2)]
+c1UA_to_m = 14960*10^11; %[m]
+c1mUA_to_m = c1UA_to_m/1000; %[m]
+c1Ano_to_s = 3600*24*365; %[s]
+GM = GM*c1Ano_to_s^2/c1mUA_to_m^3 ;%[UA^3/(kg*Ano^2)]
 
-xp = 8.76656*10^14;
+% xp = 8.76656*10^14;
+% yp = 0;
+
+xp = 586; %[mUA]
 yp = 0;
-vpy = 70.56;
+vpy = 70.56; %[m/s]
+vpy = vpy * c1Ano_to_s/c1mUA_to_m ;%[mUA/Ano]
 vpx = 0;
 Y(:, 1) = [ xp; yp; vpx; vpy];
 
--- Y_dot.m
+++ Y_dot.m
@@ -3,9 +3,17 @@ function A = Y_dot( x, y )
 %   Detailed explanation goes here
     G = 6.674287*10.^(-11);
     M = 1.9891*10.^30;
-    xp = 8.76656*10^14;
+    % xp = 8.76656*10^14;
+    % yp = 0;
+    GM = G*M; %[m^3/(kg*s^2)]
+    c1UA_to_m = 14960*10^11; %[m]
+    c1mUA_to_m = c1UA_to_m/1000; %[m]
+    c1Ano_to_s = 3600*24*365; %[s]
+    GM = GM*c1Ano_to_s^2/c1mUA_to_m^3; %[UA^3/(kg*Ano^2)]
+    xp = 586; %[mUA]
     yp = 0;
-    vpy = 70.56;
+    vpy = 70.56; %[m/s]
+    vpy = vpy * c1Ano_to_s/c1mUA_to_m ;%[mUA/Ano]
     vpx = 0;
     r = x.^2 + y.^2;
     rp = xp.^2 + yp.^2;
@@ -13,16 +21,16 @@ function A = Y_dot( x, y )
         A = [
             vpx;
             vpy;
-            -G*M/sqrt(rp^3);
+            -GM/sqrt(rp^3);
             0
             ];
     else
         A = [
-            -G*M*(r)/sqrt(r^3)+G*M*rp/sqrt(rp^3) + vpx;
-            -G*M*(r)/sqrt(r^3)+G*M*rp/sqrt(rp^3) + vpy;
-            G*M*x/sqrt(r^3);
-            G*M*y/sqrt(r^3)
-            ];
+            -GM*r/sqrt(r^3)+GM*rp/sqrt(rp^3) + vpx;
+            -GM*r/sqrt(r^3)+GM*rp/sqrt(rp^3) + vpy;
+            -GM*x/sqrt(r^3);
+            GM*y/sqrt(r^3)
+            ]
 
     end
 
