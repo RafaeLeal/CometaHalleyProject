2ed4032c7613682b2c1033f326346c990b1fad95
Rafael <rafaelealdias@gmail.com>
2016-04-03 21:46:29 -0300

    Funciona!!

--- Main2.m
+++ Main2.m
@@ -1,15 +1,22 @@
 clear all
 close all
+perihelio = 587; % [mUA]
+aphelio = 35110; % [mUA]
+semimajor = 17850; % [mUA]
+%Discretização do tempo
 t0 = 0;
-tf = 100;
-n = 300;
+tf = 85000000;
+n = 2000000;
 dt = (tf - t0)/n;
+
+%Valores Iniciais 
 %u1 = x
 %u2 = vx
 %v1 = y
 %v2 = vy
-
-Y_0  = [10; 0; 0; 1]
+%Y_0 = [x, vx, y, vy]
+%Y_0  = [10; 0; 0; 1] % Teste
+Y_0 = [587; 0; 0; 0.0115]
 all_Y(:, 1) = Y_0
 Y = Y_0;
 for i = 1:n
--- Y_dot2.m
+++ Y_dot2.m
@@ -1,12 +1,10 @@
-function A = Y_dot2(u1, u2, v1, v2)
+function ANS = Y_dot2(u1, u2, v1, v2)
         
-        %G = 6.674287*10.^(-11);
-        %M = 1.9891*10.^30;
-        %GM = G*M ;%[m^3/(kg*s^2)]    
-        GM = 10;
+        GM = 0.0394348259562330; % [mUA^3]/[Ano^2]
+        %GM = 10; %Teste
         r = sqrt(u1^2+v1^2);
-        [-GM*u1/r^3 , -GM*u1/r^3]
-        A = [
+        a = [-GM*u1/r^3 , -GM*u1/r^3];
+        ANS = [
             u2;         %u1_dot = u2
             -GM*u1/r^3;  %u2_dot
             v2;         %v1_dot = v2
